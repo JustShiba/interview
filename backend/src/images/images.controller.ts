603dc5b07fb7e4b75896f9daa5b3846a1425363d
JustShiba <liyalisica9356@gmail.com>
2023-05-03 00:20:08 +0300

    feat: add endpoints

--- backend/src/images/images.controller.ts
+++ backend/src/images/images.controller.ts
@@ -1,34 +1,93 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import * as path from 'path';
+
+import {
+  BadRequestException,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { diskStorage, Multer } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+
 import { ImagesService } from './images.service';
-import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
 
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
   @Post()
-  create(@Body() createImageDto: CreateImageDto) {
-    return this.imagesService.create(createImageDto);
+  @HttpCode(200)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: path.resolve('images'),
+        filename: (req, file, callback) => {
+          const { originalname } = file;
+
+          const ext = path.extname(originalname);
+          callback(null, `${Date.now()}_${originalname.split('.')[0]}${ext}`);
+        },
+      }),
+      limits: {
+        fileSize: 10 * 1024 * 1024,
+      },
+    }),
+  )
+  async create(
+    @UploadedFile() file: Multer.File,
+  ): Promise<{ filename: string }> {
+    if (!file) {
+      throw new BadRequestException('No image provided');
+    }
+
+    return { filename: file.filename };
   }
 
   @Get()
-  findAll() {
+  @HttpCode(200)
+  findAll(): string[] {
     return this.imagesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.imagesService.findOne(+id);
+  @Get(':filename')
+  @HttpCode(200)
+  findOne(@Param('filename') filename: string, @Res() res: Response): void {
+    this.imagesService.findOne(filename, res);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateImageDto: UpdateImageDto) {
-    return this.imagesService.update(+id, updateImageDto);
+  @Patch(':filename')
+  @HttpCode(200)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      limits: {
+        fileSize: 10 * 1024 * 1024,
+      },
+    }),
+  )
+  async update(
+    @Param('filename') filename: string,
+    @UploadedFile() file: Multer.File,
+  ): Promise<{ filename: string }> {
+    if (!file) {
+      throw new BadRequestException('No image provided');
+    }
+
+    await this.imagesService.update(filename, file);
+
+    return { filename: filename };
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.imagesService.remove(+id);
+  @Delete(':filename')
+  @HttpCode(200)
+  remove(@Param('filename') filename: string): Promise<void> {
+    return this.imagesService.remove(filename);
   }
 }
